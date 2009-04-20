11eae4e0d90008d8bfe481a38f9cacb106a31681
luizfilipefc <luizfilipefc@94ee1c2a-1fe1-11de-a027-6f5b8e30e268>
2009-04-20 14:53:47 +0000

    Exibição de mensagens confortáveis ao usuário.

--- trunk/web/javascript/library/Application.js
+++ trunk/web/javascript/library/Application.js
@@ -1,34 +1,51 @@
 /**
  * @author Gean Junior
  */
-Application = function Application() {
+Application = function Application() {}
 
-}
+Application.CONTENT = "#content"
+Application.DEFAULT_PAGE = "view/default.php";
+Application.MAIN_CONTROL = "controller/main.control.php";
+
+Application.messages = new ApplicationMessages();
 
 Application.prototype.login = function login(user, password) {
-	$.getJSON("controller/login.control.php?user=" + user + "&password=" + password,
-		function(data) {
-			if(data.logged) {
-				$.get("view/default.php", function(data) {
-					$("#content").html(data);
-				});
-			} else {
-				alert("Usuário ou senha inválido!")
-			}
+	Application.messages.showMessage("Autenticando...");
+	
+	var data = { user:user, password:password };
+	var callback = function(json) {
+		Application.messages.hideMessage();
+		if(json.logged) {
+			$.get(Application.DEFAULT_PAGE, function(data) {
+				$(Application.CONTENT).html(data);
+			});
+		} else {
+			alert("Usuário ou senha inválido!");
 		}
-	);
+	}
+	
+	$.getJSON("controller/login.control.php", data, callback );
 }
 
 Application.prototype.openPage = function(url, target) {
-	$.get("controller/main.control.php",	
+	Application.messages.showMessage("Abrindo página...");
+	
+	$.get(Application.MAIN_CONTROL,	
 		{page: url, target: target},
 		function(data) {
+			Application.messages.hideMessage();
 			$(target).html(data);
 		}
 	);
 }
 
-Application.prototype.sendData = function sendData(control, params, callback) {
+Application.prototype.sendjson = function sendjson(control, params, callback) {
+	Application.messages.showMessage("Requisitando...");
+	var customCallback = function(data) {
+		Application.messages.hideMessage();
+		callback(data);
+	}
+	
 	if(!callback) {
 		callback = function(data) {
 			alert(data);
@@ -38,24 +55,24 @@ Application.prototype.sendData = function sendData(control, params, callback) {
 	var url = "controller/" + control;
 	
 	//faz requisição ao servidor utilizado o método post e recebendo uma resposta em json
-	$.post(url, params, callback, "json");
+	$.post(url, params, customCallback, "json");
 }
 
 Application.prototype.sendForm = function sendForm(form, callback) {
-	this.sendData($("#"+form).attr("action"), this.readFromForm(form), callback);
+	this.sendjson($("#"+form).attr("action"), this.readFromForm(form), callback);
 }
 
 Application.prototype.readFromForm = function readFromForm(form) {
 	var form = $("#" + form + " input, form select, form textarea");
 
-	var data = "{";
-	if( form.length > 0 && form[0].name ) data += form[0].name + ": '" + form[0].value + "'";
+	var json = "{";
+	if( form.length > 0 && form[0].name ) json += form[0].name + ": '" + form[0].value + "'";
 	for(var i = 1; i < form.length; i++) {
-		if( form[i].name ) data += ", " + form[i].name + ": '" + URLEncode(form[i].value) + "'";
+		if( form[i].name ) json += ", " + form[i].name + ": '" + URLEncode(form[i].value) + "'";
 	}
-	data += "}";
+	json += "}";
 
-	var obj = eval("(" + data + ")");
+	var obj = eval("(" + json + ")");
 	return obj;
 }
 
